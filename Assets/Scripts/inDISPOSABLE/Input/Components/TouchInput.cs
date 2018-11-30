f5281339fda07d3404ff71f333a8d58bf3fcf627
Anthony Capobianchi <anthony.capobianchi@gmail.com>
2018-11-30 16:25:12 -0500

    added capture components for touch input

---
+++ Assets/Scripts/inDISPOSABLE/Input/Components/TouchInput.cs
@@ -0,0 +1,10 @@
+namespace Indisposable.Input
+{
+    using UnityEngine;
+    using Unity.Mathematics;
+
+    public class TouchInput : MonoBehaviour
+    {
+        public float2 Value;
+    }
+}
---
+++ Assets/Scripts/inDISPOSABLE/Input/Systems/TouchInputSystem.cs
@@ -0,0 +1,46 @@
+namespace Indisposable.Input
+{
+    using UnityEngine;
+    using Unity.Entities;
+    using Unity.Mathematics;
+
+    [ UpdateAfter( typeof( TouchSystem ) ) ]
+    public class TouchInputSystem : ComponentSystem
+    {
+        private struct TouchInputEntityFilter
+        {
+            public readonly Touch TouchComponent;
+            public TouchInput InputComponent;
+        }
+
+        protected override void OnUpdate()
+        {
+            foreach( TouchInputEntityFilter entity in GetEntities<TouchInputEntityFilter>() )
+            {
+                Touch touch = entity.TouchComponent;
+                TouchInput touchInput = entity.InputComponent;
+
+                touchInput.Value = float2.zero;
+
+                if( touch.Phase == TouchPhase.Stationary || touch.Phase == TouchPhase.Moved )
+                {
+                    float horizontalDistance = math.abs( touch.StartPosition.x - touch.EndPosition.x );
+                    float verticalDistance = math.abs( touch.StartPosition.y - touch.EndPosition.y );
+
+                    Debug.Log( horizontalDistance + " : " + verticalDistance );
+
+                    float horizontalInput = 0;
+                    float verticalInput = 0;
+
+                    if( horizontalDistance >= 30f )
+                        horizontalInput = math.sign( touch.EndPosition.x - touch.StartPosition.x );
+
+                    if( verticalDistance >= 30f )
+                        verticalInput = math.sign( touch.EndPosition.y - touch.StartPosition.y );
+
+                    touchInput.Value = new float2( horizontalInput, verticalInput );
+                }                    
+            }
+        }
+    }
+}
